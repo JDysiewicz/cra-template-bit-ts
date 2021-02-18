5dc931475fd92fc74d804cb7114b174565938e74
JDysiewicz <joshuadysiewicz@googlemail.com>
2021-02-18 10:00:05 +0000

    Initial draft V1.0.0

---
+++ template/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	tabWidth: 4, // tab equivalent to 4 spaces
+	semi: true, // automatically add semicolons
+	singleQuote: false, // Converts single quote strings to double quotes
+	useTabs: true, // converts spaces to tabs for indentation
+	trailingComma: "all", // adds comma on last property of an object
+};
